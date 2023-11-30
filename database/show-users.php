3de99bf56a5709c2c09e17b4e240b02c05078aba
Alessandro Maria Mezzina <a.mezzina5@studenti.poliba.it>
2023-11-30 12:42:14 +0100

    Mattina jquery

---
+++ database/show-users.php
@@ -0,0 +1,8 @@
+<?php
+include('connection.php');
+
+$stmt = $conn->prepare("SELECT * FROM users ORDER BY created_at DESC");
+$stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+return $stmt->FetchAll();
--- users-add.php
+++ users-add.php
@@ -3,7 +3,7 @@
     if(!isset($_SESSION['user'])) header('location: login.php');
     $_SESSION['table'] = 'users';
     $user = $_SESSION['user'];
-
+    $users = include('database/show-users.php')
 ?>
 
 <!DOCTYPE html>
@@ -14,51 +14,124 @@
         <script src="http://use.fontawesome.com/0c7a3095b5.js"></script>
     </head>
 
-    <body>
-        <div id="dashboardMainContainer">
-            <?php include('partials/app-sidebar.php') ?>
-            <div class="dashboard_content_container" id="dashboard_content_container">
-                <?php include('partials/app-topnav.php') ?>
-                    <div class="dashboard_content">
-                        <div class="dashboard_content_main">
+<body>
+    <div id="dashboardMainContainer">
+        <?php include('partials/app-sidebar.php') ?>
+        <div class="dashboard_content_container" id="dashboard_content_container">
+            <?php include('partials/app-topnav.php') ?>
+            <div class="dashboard_content">
+                <div class="dashboard_content_main">
+                    <div class="row">
+                        <div class="column column-5">  
+                            <h1 class="section_header"><i class="fa fa-plus"></i> Create User</h1>    
                             <div id="userAddFormContainer">
-                            <form action="database/add.php" method="POST" class="appForm">
-                                <div class="appFormInputContainer">
-                                    <label for="first_name">First Name</label>
-                                    <input type="text" class="appFormInput" id="first_name" name="first_name"/>
-                                </div>
-                                <div class="appFormInputContainer">
-                                    <label for="last_name">Last Name</label>
-                                    <input type="text" class="appFormInput" id="last_name" name="last_name"/>
-                                </div>
-                                <div class="appFormInputContainer">
-                                    <label for="email">Email</label>
-                                    <input type="text" class="appFormInput" id="email" name="email"/>
-                                </div>
-                                <div class="appFormInputContainer">
-                                    <label for="password">Password</label>
-                                    <input type="password" class="appFormInput" id="password" name="password"/>
-                                </div>
-                                <button type="submit" class="appBtn"><i class="fa fa-plus"></i> Add User</button>
-                            </form>
+                                <form action="database/add.php" method="POST" class="appForm">
+                                    <div class="appFormInputContainer">
+                                        <label for="first_name">First Name</label>
+                                        <input type="text" class="appFormInput" id="first_name" name="first_name"/>
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="last_name">Last Name</label>
+                                        <input type="text" class="appFormInput" id="last_name" name="last_name"/>
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="email">Email</label>
+                                        <input type="text" class="appFormInput" id="email" name="email"/>
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="appFormInput" id="password" name="password"/>
+                                    </div>
+                                    <button type="submit" class="appBtn"><i class="fa fa-plus"></i> Add User</button>
+                                </form>
 
-                            <?php 
-                                if(isset($_SESSION['response'])){
-                                    $response_message = $_SESSION['response']['message'];
-                                    $is_success = $_SESSION['response']['success'];
-                            ?>
-                                <div class="responseMessage">
-                                    <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
-                                    <?= $response_message ?>
-                                    </p>
+                                <?php 
+                                    if(isset($_SESSION['response'])){
+                                        $response_message = $_SESSION['response']['message'];
+                                        $is_success = $_SESSION['response']['success'];
+                                ?>
+                                    <div class="responseMessage">
+                                        <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
+                                            <?= $response_message ?>
+                                        </p>
+                                    </div>
+                                <?php unset($_SESSION['response']); } ?>
+                            </div>
+                        </div>
+                        <div class="column column-7">
+                            <h1 class="section_header"><i class="fa fa-list"></i> List of Users</h1> 
+                            <div class="section_content">
+                                <div class="users">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>First Name</th>
+                                                <th>Last Name</th>
+                                                <th>Email</th>
+                                                <th>Created at</th>
+                                                <th>Updated at</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach($users as $index => $user){ ?>
+                                                <tr>
+                                                    <td><?= $index + 1 ?></td>
+                                                    <td><?= $user['first_name'] ?></td>
+                                                    <td><?= $user['last_name'] ?></td>
+                                                    <td><?= $user['email'] ?></td>
+                                                    <td><?= date('M d,Y @ h:i:s A', strtotime( $user['created_at'])) ?></td>
+                                                    <td><?= date('M d,Y @ h:i:s A', strtotime( $user['updated_at'])) ?></td>
+                                                    <td>
+                                                        <a href=""><i class="fa fa-pencil"></i> Edit</a>
+                                                        <a href="" class="deleteUser" data-userid="<?= $user['id'] ?>" data-fname="<?= $user['first_name'] ?>" data-lname="<?= $user['last_name'] ?>"><i class="fa fa-trash"></i> Delete</a>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>    
+                                        </tbody>
+                                    </table>
+                                    <p class="userCount"><?= count($users) ?> Users </p>
                                 </div>
-                            <?php unset($_SESSION['response']); } ?>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div>    
             </div>
         </div>
+    </div>
 
-        <script src="js/script.js?v=<?= time(); ?>"></script>
-    </body>
+<script src="js/script.js?v=<?= time(); ?>"></script>
+<script>
+    function script(){
+
+        this.initialize = function(){
+            this.registerEvents();
+        },
+        this.registerEvents = function(){
+            document.addEventListener('click', function(e){
+                targetElement = e.target;
+                classList = targetElement.classList;
+
+                if(classList.contains('deleteUser')){
+                    e.preventDefault();
+                    userId = targetElement.dataset.userid;
+                    fname = targetElement.dataset.fname;
+                    lname = targetElement.dataset.lname;
+                    fullName = fname + ' ' + lname;
+
+                    if(window.confirm('Are you sure to delete '+ fullName + '?')){
+
+                    } else {
+                        
+                    }
+                }
+            });
+        }
+    }
+
+    var script = new script;
+    script.initialize(); 
+</script>
+</body>
 </html>
